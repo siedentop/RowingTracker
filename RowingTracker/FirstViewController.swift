81a0280ed776c3cb880d3c4b9eff35ee96e695ce
Christoph Siedentop <christoph@siedentop.name>
2014-06-05 20:50:00 +0200

    Initial Commit

---
+++ RowingTracker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  RowingTracker
+//
+//  Created by Christoph Siedentop on 05.06.14.
+//  Copyright (c) 2014 Christoph Siedentop. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RowingTracker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RowingTracker
+//
+//  Created by Christoph Siedentop on 05.06.14.
+//  Copyright (c) 2014 Christoph Siedentop. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
